a4823924b7908966d41124a25d799d3df9c0225c
Abhilash Rajan <rajan.abhilash@gmail.com>
2020-03-07 11:20:14 +0530

    Initial Commit

---
+++ Webscrapperprj/Job.py
@@ -0,0 +1,9 @@
+class Job:
+    def __init__(self, job_title, job_company, job_place, job_exp, job_pac, job_desc, job_skills):
+        self.jobTitle = job_title
+        self.jobCompany = job_company
+        self.jobPlace = job_place
+        self.jobExp = job_exp
+        self.jobPac = job_pac
+        self.jobDesc = job_desc
+        self.jobSkills = job_skills
